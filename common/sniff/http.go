a940a14edf733beaf630f925ff611c07613ae0eb
lux5am <lux.5am@yandex.com>
2024-12-03 03:41:18 +0800

    fix sniff host ignore ip
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- common/sniff/http.go
+++ common/sniff/http.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"errors"
 	"io"
+	"net/netip"
 
 	"github.com/sagernet/sing-box/adapter"
 	C "github.com/sagernet/sing-box/constant"
@@ -23,6 +24,9 @@ func HTTPHost(_ context.Context, metadata *adapter.InboundContext, reader io.Rea
 		}
 	}
 	metadata.Protocol = C.ProtocolHTTP
-	metadata.SniffHost = M.ParseSocksaddr(request.Host).AddrString()
+	host := M.ParseSocksaddr(request.Host).AddrString()
+	if _, err = netip.ParseAddr(host); err != nil {
+		metadata.SniffHost = host
+	}
 	return nil
 }
