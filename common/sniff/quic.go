47643b415b5b940c984c53632b9fbc02fe7c973c
lux5am <lux.5am@yandex.com>
2024-12-03 03:41:18 +0800

    fix sniff host ignore ip
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- common/sniff/quic.go
+++ common/sniff/quic.go
@@ -9,6 +9,7 @@ import (
 	"encoding/binary"
 	"io"
 	"os"
+	"net/netip"
 
 	"github.com/sagernet/sing-box/adapter"
 	"github.com/sagernet/sing-box/common/ja3"
@@ -308,7 +309,9 @@ find:
 		metadata.SniffContext = fragments
 		return E.Cause1(ErrNeedMoreData, err)
 	}
-	metadata.SniffHost = fingerprint.ServerName
+	if _, err = netip.ParseAddr(fingerprint.ServerName); err != nil {
+		metadata.SniffHost = fingerprint.ServerName
+	}
 	for metadata.Client == "" {
 		if len(frameTypeList) == 1 {
 			metadata.Client = C.ClientFirefox
