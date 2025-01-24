41159a813b82ec51eb4753675aab33e27935bdee
lux5am <lux.5am@yandex.com>
2025-01-24 08:35:54 +0800

    add sniff override destination to rule action
    Signed-off-by: lux5am <lux.5am@yandex.com>

--- route/rule/rule_action.go
+++ route/rule/rule_action.go
@@ -89,6 +89,7 @@ func NewRuleAction(ctx context.Context, logger logger.ContextLogger, action opti
 		sniffAction := &RuleActionSniff{
 			snifferNames: action.SniffOptions.Sniffer,
 			Timeout:      time.Duration(action.SniffOptions.Timeout),
+			OverrideDestination: action.SniffOptions.OverrideDestination,
 		}
 		return sniffAction, sniffAction.build()
 	case C.RuleActionTypeResolve:
@@ -365,7 +366,7 @@ type RuleActionSniff struct {
 	StreamSniffers []sniff.StreamSniffer
 	PacketSniffers []sniff.PacketSniffer
 	Timeout        time.Duration
-	// Deprecated
+
 	OverrideDestination bool
 }
 
