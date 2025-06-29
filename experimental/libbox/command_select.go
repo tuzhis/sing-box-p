tukimax/sing-box

add outbound provider to provide hot-update ability
Signed-off-by: lux5am <lux.5am@yandex.com>

experimental/libbox/command_select.go
@@ -43,7 +43,7 @@ func (s *CommandServer) handleSelectOutbound(conn net.Conn) error {
 	if service == nil {
 		return writeError(conn, E.New("service not ready"))
 	}
-	outboundGroup, isLoaded := service.instance.Router().Outbound(groupTag)
+	outboundGroup, isLoaded := service.instance.Router().OutboundWithProvider(groupTag)
 	if !isLoaded {
 		return writeError(conn, E.New("selector not found: ", groupTag))
 	}

tuzhis/sing-box-p

experimental/libbox/command_select.go
@@ -43,7 +43,7 @@ func (s *CommandServer) handleSelectOutbound(conn net.Conn) error {
 	if service == nil {
 		return writeError(conn, E.New("service not ready"))
 	}
-	outboundGroup, isLoaded := service.instance.Outbound().Outbound(groupTag)
+	outboundGroup, isLoaded := service.instance.Outbound().OutboundWithProvider(groupTag)
 	if !isLoaded {
 		return writeError(conn, E.New("selector not found: ", groupTag))
 	}